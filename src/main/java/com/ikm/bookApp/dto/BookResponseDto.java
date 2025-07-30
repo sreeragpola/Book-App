97523846c6ad624cbfd6c76ab202d69f124c3626
Sreerag Pola <sreerag@kiebot.com>
2025-07-30 13:02:40 +0530

    sreerag|add book get api

--- src/main/java/com/ikm/bookApp/dto/BookResponseDto.java
+++ src/main/java/com/ikm/bookApp/dto/BookResponseDto.java
@@ -2,11 +2,18 @@ package com.ikm.bookApp.dto;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookResponseDto {
     private String isbn;
     private String title;
     private String author;
-    private int publicationYear;
+    private Long publicationYear;
     private List<Reviewdto> reviews;
     private Double averageRating;
     private int totalReviewCount;  
